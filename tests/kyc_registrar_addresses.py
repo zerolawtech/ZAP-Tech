ce3d39007ef3705a668b07d2f1565898a4d8d437
Ryan Cocks <ryan@ryancocks.net>
2019-02-18 02:04:18 +0200

    KYC: address management tests

--- tests/kyc_registrar_addresses.py
+++ tests/kyc_registrar_addresses.py
@@ -2,6 +2,8 @@
 
 from brownie import *
 
+zero32 = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+
 def setup():
     # is this passing a threshold of zero?
     global a, countries
@@ -9,15 +11,16 @@ def setup():
     a = accounts
     global owner1, owner2
     owner1 = a[0]; owner2 = a[1]
+    global authority1, authority2
+    authority1 = a[2]; authority2 = a[3]
+    global investor1, investor2
+    investor1 = a[4]; investor2 = a[5]
     global scratch1 
     scratch1 = a[-1]
 
-# TODO: restricted addresses
-# TODO: multisig for owners and authorities
-# TODO: When restricing addresses associated to an authority, you cannot reduce the number of addresses such that the total remaining is lower than the multi-sig threshold value for that authority.
 
 ################################################
-# Addresses
+# Tests for management of Addresses / ID mappings
 
 # Owners controlling owners
 
@@ -85,39 +88,56 @@ def restricted_owner_address_cant_add_a_new_owner_address(pending=True):
 
 # Owners controlling authorities
 
-def owner_can_add_a_new_authority_address(skip=True):
-    pass
+def owner_can_add_a_new_authority_address_multisig():
+    registrar = owner1.deploy(KYCRegistrar, [owner1, owner2], 2)
+    registrar.addAuthority([authority1], countries, 1, {'from':owner1})
+    check.equal(registrar.getID(authority1), zero32)
+    registrar.addAuthority([authority1], countries, 1, {'from':owner2})
+    check.not_equal(registrar.getID(authority1), zero32)
 
-def owner_can_restrict_an_authority_address(skip=True):
-    pass
+def owner_can_restrict_an_authority_address():
+    registrar = owner1.deploy(KYCRegistrar, [owner1, owner2], 1)
+    registrar.addAuthority([authority1, authority2], countries, 1, {'from':owner1})
+    authorityID = registrar.getID(authority1)
+    txr = registrar.restrictAddresses(authorityID, [authority1], {'from':owner1})
+    check.equal(txr.return_value, True)
+    check.event_fired(txr, 'RestrictedAddresses' , 1)
 
-def restricted_owner_address_cant_add_a_new_authority_address(skip=True):
-    pass
+def owner_cant_restrict_too_many_authority_addresses():
+    # "When restricing addresses associated to an authority, you cannot reduce the number
+    #  of addresses such that the total remaining is lower than the multi-sig threshold
+    #  value for that authority." 
+    registrar = owner1.deploy(KYCRegistrar, [owner1, owner2], 1)
+    registrar.addAuthority([authority1, authority2, scratch1], countries, 2, {'from':owner1})
+    authorityID = registrar.getID(authority1)
+    txr = registrar.restrictAddresses(authorityID, [authority1], {'from':owner1})
+    check.equal(txr.return_value, True)
+    check.event_fired(txr, 'RestrictedAddresses' , 1)
+    check.reverts(registrar.restrictAddresses, (authorityID, [authority1], {'from':owner1}))
 
-def restricted_owner_address_cant_restrict_an_authority_address(skip=True):
-    pass
 
 # Authorities
 
-def authority_cant_add_a_new_authority_address(skip=True):
-    # only owner can do this
-    pass
-
-def authority_cant_add_a_new_owner_address(skip=True):
-    # only owner can do this
-    pass
-
-def authority_can_add_a_new_investor_address_in_their_country(skip=True):
+def authority_can_add_a_new_investor_address_in_their_country():
     # given two authorities for country X then both can add addresses
-    # for an investor in that country
-    pass
+    # for an investor in that country - even if not the original authority
+    registrar = owner1.deploy(KYCRegistrar, [owner1, owner2], 1)
+    registrar.addAuthority([authority1], countries, 1, {'from':owner1})
+    registrar.addAuthority([authority2], countries, 1, {'from':owner1})
+    id = '0x9c93effa8c8bc959961e24dbda7cbc1be362a1df'
+    registrar.addInvestor(id, 1, 1, 1, 4550275220, [investor1], {'from':authority1})
+    id = registrar.getID(investor1)
+    registrar.registerAddresses(id, [investor2], {'from':authority2})
+    id2 = registrar.getID(investor2)
+    check.equal(id, id2)
 
 # Investors
 
-def investor_cant_add_a_new_address(skip=True):
+def investor_cant_add_a_new_address():
     # only owner/authorities can do this
-    pass
-
-
-
-
+    registrar = owner1.deploy(KYCRegistrar, [owner1, owner2], 1)
+    registrar.addAuthority([authority1], countries, 1, {'from':owner1})
+    id = '0x9c93effa8c8bc959961e24dbda7cbc1be362a1df'
+    registrar.addInvestor(id, 1, 1, 1, 4550275220, [investor1], {'from':authority1})
+    id = registrar.getID(investor1)
+    check.reverts(registrar.registerAddresses, (id, [investor2], {'from':investor1}))

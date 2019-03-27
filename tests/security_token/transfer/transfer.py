7f5d674b81f033dac747e0cff4d995019868e78f
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2019-03-27 19:11:53 +0000

    total investor limit: rating

--- tests/security_token/transfer/transfer.py
+++ tests/security_token/transfer/transfer.py
@@ -13,8 +13,8 @@ def setup():
     token.mint(issuer, 1000000, {'from': a[0]})
     issuer.setCountries(
         [1, 2, 3, 4, 5],    # country
-        [1, 1, 1, 2, 2],    # minRating
-        [1, 2, 2, 3, 3],    # limit
+        [1, 1, 1, 1, 1],    # minRating
+        [0, 0, 0, 0, 0],    # limit
         {'from': a[0]}
     )
     issuer.setInvestorLimits([3, 2, 2, 1, 0, 0, 0, 0], {'from': a[0]})
@@ -231,3 +231,42 @@ def country_investor_limit_investor_investor_different_country():
     issuer.setCountry(1, True, 1, [1,0,0,0,0,0,0,0], {'from': a[0]})
     token.transfer(a[1], 1000, {'from': a[0]})
     token.transfer(a[3], 500, {'from': a[1]})
+
+def total_investor_limit_rating_blocked_issuer_investor():
+    '''total investor limit, rating - blocked, issuer to investor'''
+    issuer.setInvestorLimits([0,1,0,0,0,0,0,0], {'from':a[0]})
+    token.transfer(a[1], 1000, {'from': a[0]})
+    check.reverts(
+        token.transfer,
+        (a[3], 1000, {'from': a[0]}),
+        "Total Investor Limit: Rating"
+    )
+
+def total_investor_limit_rating_blocked_investor_investor():
+    '''total investor limit, rating - blocked, investor to investor'''
+    issuer.setInvestorLimits([0,1,0,0,0,0,0,0], {'from':a[0]})
+    token.transfer(a[1], 1000, {'from': a[0]})
+    check.reverts(
+        token.transfer,
+        (a[3], 500, {'from': a[1]}),
+        "Total Investor Limit: Rating"
+    )
+
+def total_investor_limit_rating_issuer_investor():
+    '''total investor limit, rating - issuer to existing investor'''
+    issuer.setInvestorLimits([0,1,0,0,0,0,0,0], {'from':a[0]})
+    token.transfer(a[1], 1000, {'from': a[0]})
+    token.transfer(a[1], 1000, {'from': a[0]})
+
+def total_investor_limit_rating_investor_investor():
+    '''total investor limit, rating - investor to investor, full balance'''
+    issuer.setInvestorLimits([0,1,0,0,0,0,0,0], {'from':a[0]})
+    token.transfer(a[1], 1000, {'from': a[0]})
+    token.transfer(a[2], 1000, {'from': a[1]})
+
+def total_investor_limit_rating_investor_investor_different_country():
+    '''total investor limit, rating - investor to investor, different rating'''
+    issuer.setInvestorLimits([0,1,0,0,0,0,0,0], {'from':a[0]})
+    token.transfer(a[1], 1000, {'from': a[0]})
+    token.transfer(a[2], 500, {'from': a[1]})
+

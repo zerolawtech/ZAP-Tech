ba65a1cf1798bc50913fed99dfeee269aae536c0
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2019-04-04 20:14:31 +0300

    more merge tests

--- tests/nft/transfer_range/merges.py
+++ tests/nft/transfer_range/merges.py
@@ -222,6 +222,115 @@ def whole_range_right():
     )
 
 
+def inside_one():
+    '''inside, one token'''
+    token.transferRange(a[4], 12000, 12001, {'from': a[2]})
+    _check(
+        [(1,10001)],
+        [(10001, 12000),(12001,20001)],
+        [(20001, 30001)],
+        [(12000, 12001)]
+    )
+
+
+def one_left():
+    '''one token, touch left'''
+    token.transferRange(a[4], 10001, 10002, {'from': a[2]})
+    _check(
+        [(1,10001)],
+        [(10002, 20001)],
+        [(20001, 30001)],
+        [(10001, 10002)]
+    )
+
+def one_left_abs():
+    '''one token, touch left, absolute'''
+    token.transferRange(a[4], 1, 2, {'from': a[1]})
+    _check(
+        [(2,10001)],
+        [(10001, 20001)],
+        [(20001, 30001)],
+        [(1, 2)]
+    )
+
+def one_left_merge():
+    '''one token, touch left, merge'''
+    token.transferRange(a[4], 1, 5000, {'from': a[1]})
+    token.transferRange(a[1], 10001, 10002, {'from': a[2]})
+    _check(
+        [(5000,10002)],
+        [(10002, 20001)],
+        [(20001, 30001)],
+        [(1, 5000)]
+    )
+
+def one_left_merge_abs():
+    '''one token, touch left, merge, absolute'''
+    token.transferRange(a[1], 10001, 10002, {'from': a[2]})
+    _check(
+        [(1,10002)],
+        [(10002, 20001)],
+        [(20001, 30001)],
+        []
+    )
+
+def one_right():
+    '''one token, touch right'''
+    token.transferRange(a[4], 20000, 20001, {'from': a[2]})
+    _check(
+        [(1,10001)],
+        [(10001, 20000)],
+        [(20001, 30001)],
+        [(20000, 20001)]
+    )
+
+def one_right_abs():
+    '''one token, touch right, absolute'''
+    token.transferRange(a[4], 30000, 30001, {'from': a[3]})
+    _check(
+        [(1,10001)],
+        [(10001, 20001)],
+        [(20001, 30000)],
+        [(30000, 30001)]
+    )
+
+
+def one_right_merge():
+    '''one token touch right, merge'''
+    token.transferRange(a[4], 25000, 30001, {'from': a[3]})
+    token.transferRange(a[3], 20000, 20001, {'from': a[2]})
+    _check(
+        [(1,10001)],
+        [(10001, 20000)],
+        [(20000, 25000)],
+        [(25000, 30001)]
+    )
+
+def one_right_merge_abs():
+    '''one token, touch right, merge absolute'''
+    token.transferRange(a[3], 20000, 20001, {'from': a[2]})
+    _check(
+        [(1,10001)],
+        [(10001, 20000)],
+        [(20000, 30001)],
+        []
+    )
+
+
+def upper_abs():
+    '''upper absolute'''
+    token.modifyAuthorizedSupply(2**49, {'from': a[0]})
+    token.mint(a[4],2**48-30002, 0, "0x00", {'from': a[0]})
+    token.transferRange(a[3], 2**48-100, 2**48-1, {'from': a[4]})
+    _check(
+        [(1, 10001)],
+        [(10001, 20001)],
+        [(20001, 30001), (2**48-100, 2**48-1)],
+        [(30001, 2**48-100)]
+    )
+
+
+
 def _check(*expected_ranges):
     for num, expected in enumerate(expected_ranges, start=1):
         account = a[num]
@@ -232,7 +341,10 @@ def _check(*expected_ranges):
             sum((i[1] - i[0]) for i in ranges)
         )
         for start, stop in ranges:
-            for i in range(max(1, start-2),start+2):
+            if stop-start == 1:
+                check.equal(token.getRange(start)[:3],(account, start, stop))
+                continue
+            for i in range(max(1, start-1),start+2):
                 try:
                     data = token.getRange(i)
                 except:
@@ -243,11 +355,12 @@ def _check(*expected_ranges):
                 else:
                     check.true(data[0] == account)
                     check.true(data[2] == stop)
-            for i in range(stop-2,min(stop+2,upper)):
+            for i in range(stop-1,min(stop+1,upper)):
                 data = token.getRange(i)
                 if i < stop:
                     check.true(data[0] == account)
                     check.true(data[1] == start)
                 else:
+
                     check.true(data[0] != account)
                     check.true(data[1] == stop)

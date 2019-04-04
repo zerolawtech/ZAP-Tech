bc62862a3e163df0981c408b4145ca7c0dcb10d3
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2019-04-04 04:12:01 +0300

    move custodian tests to security_token

--- tests/security_token/transfer_custodian/transfer_from.py
+++ tests/security_token/transfer_custodian/transfer_from.py
@@ -6,19 +6,18 @@ from scripts.deployment import main
 
 def setup():
     config['test']['always_transact'] = False
-    config['test']['default_contract_owner'] = True
     main(SecurityToken)
     global token, issuer, cust
     token = SecurityToken[0]
     issuer = IssuingEntity[0]
-    cust = OwnedCustodian.deploy(a[0], [a[0]], 1)
-    issuer.addCustodian(cust)
-    token.mint(issuer, 100000)
+    cust = OwnedCustodian.deploy(a[0], [a[0]], 1, {'from': a[0]})
+    issuer.addCustodian(cust, {'from': a[0]})
+    token.mint(issuer, 100000, {'from': a[0]})
 
 
 def issuer_txfrom():
     '''Issuer transferFrom custodian'''
-    token.transfer(a[1], 10000)
+    token.transfer(a[1], 10000, {'from': a[0]})
     token.transfer(cust, 10000, {'from': a[1]})
     token.transferFrom(cust, a[1], 5000, {'from': a[0]})
     check.equal(token.balanceOf(a[1]), 5000)
@@ -27,7 +26,7 @@ def issuer_txfrom():
 
 def investor_txfrom():
     '''Investor transferFrom custodian'''
-    token.transfer(a[1], 10000)
+    token.transfer(a[1], 10000, {'from': a[0]})
     token.transfer(cust, 10000, {'from': a[1]})
     check.reverts(
         token.transferFrom,

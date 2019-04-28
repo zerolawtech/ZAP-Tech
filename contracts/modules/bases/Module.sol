e8c70052249c19d9355c1c1574a57b5d1cb6be34
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-04-28 13:08:52 +0400

    add ModuleBaseABC

--- contracts/modules/bases/Module.sol
+++ contracts/modules/bases/Module.sol
@@ -4,22 +4,21 @@ import "../../SecurityToken.sol";
 import "../../bases/MultiSig.sol";
 
 /**
+	@title ModuleBase Abstract Base Contract
+	@dev Methods in this ABC are defined in contracts that inherit ModuleBase
+*/
+contract ModuleBaseABC {
+	function getPermissions() external pure returns (bytes4[], bytes4[], uint256);
+}
+
+/**
 	@title Module Base Contract
 	@dev Inherited contract for IssuingEntity or Custodian modules
 */
-contract ModuleBase {
+contract ModuleBase is ModuleBaseABC {
 
 	bytes32 public ownerID;
 	address owner;
-
-	/** @dev Check that call originates from approved authority, allows multisig */
-	function _onlyAuthority() internal returns (bool) {
-		return MultiSig(owner).checkMultiSigExternal(
-			msg.sender,
-			keccak256(msg.data),
-			msg.sig
-		);
-	}
 
 	/**
 		@notice Base constructor
@@ -28,6 +27,15 @@ contract ModuleBase {
 	constructor(address _owner) public {
 		owner = _owner;
 		ownerID = MultiSig(owner).ownerID();
+	}
+
+	/** @dev Check that call originates from approved authority, allows multisig */
+	function _onlyAuthority() internal returns (bool) {
+		return MultiSig(owner).checkMultiSigExternal(
+			msg.sender,
+			keccak256(msg.data),
+			msg.sig
+		);
 	}
 
 	/**
@@ -49,11 +57,6 @@ contract STModuleBase is ModuleBase {
 	SecurityToken public token;
 	IssuingEntity public issuer;
 
-	/** @dev Check that call originates from issuer or token contract */
-	function _onlyOwner() internal view {
-		require(msg.sender == address(token) || msg.sender == address(owner));
-	}
-
 	/**
 		@notice Base constructor
 		@param _token SecurityToken contract address
@@ -62,6 +65,11 @@ contract STModuleBase is ModuleBase {
 	constructor(address _token, address _issuer) public ModuleBase(_issuer) {
 		token = SecurityToken(_token);
 		issuer = IssuingEntity(_issuer);
+	}
+
+	/** @dev Check that call originates from issuer or token contract */
+	function _onlyOwner() internal view {
+		require(msg.sender == address(token) || msg.sender == address(owner));
 	}
 
 	/**

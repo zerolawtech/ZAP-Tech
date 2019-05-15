4aaa7dfe18cb5cf14d70e15b35b67237b1d57df2
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-05-15 23:07:00 +0300

    multicheckpoint

--- contracts/modules/bases/MultiCheckpoint.sol
+++ contracts/modules/bases/MultiCheckpoint.sol
@@ -1,46 +1,43 @@
 pragma solidity >=0.4.24 <0.5.0;
 
 import "../../open-zeppelin/SafeMath.sol";
-import "./Module.sol";
 import "../../bases/Token.sol";
+import "./Module.sol";
+
 
 /**
-    @title Checkpoint Module Base Contract
-    @dev Inherited contract for token modules requiring a balance checkpoint
+    @title MultiCheckpoint Module
+    @dev Issuer level module used to record and access checkpoints for many tokens
 */
-contract MultiCheckpointModuleBase is ModuleBase {
+contract MultiCheckpointModule is IssuerModuleBase {
 
     using SafeMath for uint256;
 
     struct Checkpoint {
-
-        uint128 previous;
-        uint128 next;
         uint256 totalSupply;
-
+        uint64 previous;
+        uint64 next;
+        bool set;
         mapping (address => uint256) balances;
         mapping (address => bool) zeroBalances;
         mapping (address => mapping(address => uint256)) custBalances;
         mapping (address => mapping(address => bool)) custZeroBalances;
-
     }
 
     struct EpochPointers {
-        uint128 previous;
-        uint128 next;
+        uint64 previous;
+        uint64 next;
     }
 
     mapping (address => EpochPointers) pointers;
-
     mapping (address => mapping(uint256 => Checkpoint)) checkpointData;
-
 
 
     /**
         @notice Base constructor
         @param _owner IssuingEntity contract address
      */
-    constructor(address _owner) ModuleBase(_owner) public {
+    constructor(address _owner) IssuerModuleBase(_owner) public {
         return;
     }
 
@@ -70,14 +67,38 @@ contract MultiCheckpointModuleBase is ModuleBase {
         return (permissions, hooks, ~uint256(0));
     }
 
+    function totalSupplyAt(
+        address _token,
+        uint256 _time
+    )
+        external
+        view
+        returns (uint256)
+    {
+        _advanceCheckpoints(_token);
+        require(checkpointData[_token][_time].set);
+        return checkpointData[_token][_time].totalSupply;
+    }
+
     /**
-        @notice Internal getter for inherited contract to query checkpoint balance
-        @dev Should only be called when now > time
+        @notice Query token checkpoint balance
+        @param _token Token contract address
         @param _owner Address of balance to query
-        @return uint256 balance
+        @param _time Checkpoint time
+        @return uint256 balance at checkpoint
      */
-    function _getBalance(TokenBase _token, address _owner, uint256 _time) internal returns (uint256) {
+    function balanceAt(
+        TokenBase _token,
+        address _owner,
+        uint256 _time
+    )
+        external
+        view
+        returns (uint256)
+    {
+        _advanceCheckpoints(_token);
         Checkpoint storage c = checkpointData[_token][_time];
+        require(c.set);
         if (c.balances[_owner] > 0) return c.balances[_owner];
         if (c.zeroBalances[_owner]) return 0;
         uint256 _value = _token.balanceOf(_owner);
@@ -86,23 +107,29 @@ contract MultiCheckpointModuleBase is ModuleBase {
     }
 
     /**
-        @notice Getter, Inherited contract query custodied checkpoint balance
-        @dev Should only be called when now > time
+        @notice Query token checkpoint balance
+        @param _token Token contract address
         @param _owner Address of balance to query
         @param _cust Custodian address
-        @return uint256 balance
+        @param _time Checkpoint time
+        @return uint256 balance at checkpoint
      */
-    function _getCustodianBalance(
+    function custodianBalanceAt(
         TokenBase _token,
         address _owner,
         address _cust,
         uint256 _time
     )
-        internal
+        external
+        view
         returns (uint256)
     {
+        _advanceCheckpoints(_token);
         Checkpoint storage c = checkpointData[_token][_time];
-        if (c.custBalances[_owner][_cust] > 0) return c.custBalances[_owner][_cust];
+        require(c.set);
+        if (c.custBalances[_owner][_cust] > 0) {
+            return c.custBalances[_owner][_cust];
+        }
         if (c.custZeroBalances[_owner][_cust]) return 0;
         uint256 _value = _token.custodianBalanceOf(_owner, _cust);
         _setCustodianBalance(c, _owner, _cust, _value);
@@ -111,10 +138,17 @@ contract MultiCheckpointModuleBase is ModuleBase {
 
     /**
         @notice Store a checkpoint balance
+        @param c Checkpoint storage pointer
         @param _owner Address to set
         @param _value Balance at address
      */
-    function _setBalance(Checkpoint storage c, address _owner, uint256 _value) private {
+    function _setBalance(
+        Checkpoint storage c,
+        address _owner,
+        uint256 _value
+    )
+        private
+    {
         while (true) {
             if (c.balances[_owner] > 0 || c.zeroBalances[_owner]) return;
             if (_value == 0) {
@@ -129,6 +163,7 @@ contract MultiCheckpointModuleBase is ModuleBase {
 
     /**
         @notice Store custodian checkpoint balance after tokens are received
+        @param c Checkpoint storage pointer
         @param _owner Address of owner
         @param _cust Address of custodian
         @param _value Amount transferred
@@ -142,7 +177,10 @@ contract MultiCheckpointModuleBase is ModuleBase {
         private
     {
         while (true) {
-            if (c.custBalances[_owner][_cust] > 0 || c.custZeroBalances[_owner][_cust]) return;
+            if (
+                c.custBalances[_owner][_cust] > 0 ||
+                c.custZeroBalances[_owner][_cust]
+            ) return;
             if (_value == 0) {
                 c.custZeroBalances[_owner][_cust] == true;
             } else {
@@ -171,14 +209,15 @@ contract MultiCheckpointModuleBase is ModuleBase {
         external
         returns (bool)
     {
-        uint256 _previous = _getLastCheckpointTime();
+        _onlyToken();
+        uint256 _previous = _advanceCheckpoints(msg.sender);
         if (_previous == 0) return true;
         Checkpoint storage c = checkpointData[msg.sender][_previous];
-        
         TokenBase _token = TokenBase(msg.sender);
-        
+        uint256 _bal;
+
         if (_rating[0] == 0 && _id[0] != ownerID) {
-            uint256 _bal = _token.custodianBalanceOf(_addr[1], _addr[0]).add(_value);
+            _bal = _token.custodianBalanceOf(_addr[1], _addr[0]).add(_value);
             _setCustodianBalance(c, _addr[1], _addr[0], _bal);
         } else {
             _bal = _token.balanceOf(_addr[0]).add(_value);
@@ -212,14 +251,15 @@ contract MultiCheckpointModuleBase is ModuleBase {
         external
         returns (bool)
     {
-        //require(msg.sender == address(token));
-        uint256 _previous = _getLastCheckpointTime();
+        _onlyToken();
+        uint256 _previous = _advanceCheckpoints(msg.sender);
         if (_previous == 0) return true;
         Checkpoint storage c = checkpointData[msg.sender][_previous];
-        
-        uint256 _bal = TokenBase(msg.sender).custodianBalanceOf(_addr[0], _cust).add(_value);
+
+        TokenBase _token = TokenBase(msg.sender);
+        uint256 _bal = _token.custodianBalanceOf(_addr[0], _cust).add(_value);
         _setCustodianBalance(c, _addr[0], _cust, _bal);
-        _bal = TokenBase(msg.sender).custodianBalanceOf(_addr[1], _cust).sub(_value);
+        _bal = _token.custodianBalanceOf(_addr[1], _cust).sub(_value);
         _setCustodianBalance(c, _addr[1], _cust, _bal);
     }
 
@@ -241,41 +281,56 @@ contract MultiCheckpointModuleBase is ModuleBase {
         external
         returns (bool)
     {
-        uint256 _previous = _getLastCheckpointTime();
+        _onlyToken();
+        uint256 _previous = _advanceCheckpoints(msg.sender);
         if (_previous == 0) return true;
         Checkpoint storage c = checkpointData[msg.sender][_previous];
         if (c.next != 0) {
-            checkpointData[msg.sender][c.next].totalSupply = c.totalSupply.add(_new).sub(_old);
+            uint256 _totalSupply = c.totalSupply.add(_new).sub(_old);
+            checkpointData[msg.sender][c.next].totalSupply = _totalSupply;
         }
         _setBalance(c, _addr, _old);
         return true;
     }
 
-    function _getLastCheckpointTime() private returns (uint256) {
-        EpochPointers storage p = pointers[msg.sender];
+    /**
+        @notice Advance token checkpoint and modify total supply
+        @param _token Token address of checkpoint to advance
+        @return epoch time of most recently passed checkpoint
+     */
+    function _advanceCheckpoints(address _token) private returns (uint256) {
+        EpochPointers storage p = pointers[_token];
         if (p.next > now || p.next == 0) {
             return p.previous;
         }
-        Checkpoint storage c = checkpointData[msg.sender][p.next];
-        uint128 _prev = p.next;
+        Checkpoint storage c = checkpointData[_token][p.next];
+        uint64 _prev = p.next;
         while (c.next != 0 && now > c.next) {
-            checkpointData[msg.sender][c.next].totalSupply = c.totalSupply;
+            checkpointData[_token][c.next].totalSupply = c.totalSupply;
             _prev = c.next;
-            c = checkpointData[msg.sender][c.next];
+            c = checkpointData[_token][c.next];
         }
-        pointers[msg.sender] = EpochPointers(_prev, c.next);
+        pointers[_token] = EpochPointers(_prev, c.next);
         return _prev;
     }
 
-    function newCheckpoint(address _token, uint128 _time) external returns (bool) {
+    /**
+        @notice Set a new checkpoint
+        @dev TODO - this shouldn't be callable by everyone
+        @param _token Token contract address to set checkpoint for
+        @param _time Epoch time to set checkpoint at
+        @return bool success
+     */
+    function newCheckpoint(address _token, uint64 _time) external returns (bool) {
         require(_time > now);
-        require(checkpointData[_token][_time].totalSupply == 0);
+        require(issuer.isActiveToken(msg.sender));
+        require(!checkpointData[_token][_time].set);
         mapping(uint256 => Checkpoint) c = checkpointData[_token];
         if (pointers[_token].next == 0 || _time < pointers[_token].next) {
             EpochPointers memory p = pointers[_token];
             pointers[_token].next = _time;
         } else {
-            uint128 _previous = pointers[_token].next;
+            uint64 _previous = pointers[_token].next;
             while (c[_previous].next != 0 && c[_previous].next < _time) {
                 _previous = c[_previous].next;
             }
@@ -283,13 +338,8 @@ contract MultiCheckpointModuleBase is ModuleBase {
         }
         c[p.previous].next = _time;
         if (p.next != 0) c[p.next].previous = _time;
-        c[_time] = Checkpoint(p.previous, p.next, 0);
+        c[_time] = Checkpoint(0, p.previous, p.next, true);
         return true;
-    }
-
-    function balanceOfAt(TokenBase _token, address _owner, uint128 _time) external view returns (uint256) {
-        _getLastCheckpointTime();
-        return _getBalance(_token, _owner, _time);
     }
 
 }

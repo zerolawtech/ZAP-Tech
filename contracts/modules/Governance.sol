75dbb9bd317424a029974d46f43ce6e7f33de35e
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-05-21 01:33:58 +0300

    governance, almost finished

--- contracts/modules/Governance.sol
+++ contracts/modules/Governance.sol
@@ -5,6 +5,7 @@ import "../open-zeppelin/SafeMath.sol";
 
 
 interface IMultiCheckpointModule {
+    function checkpointExists(address, uint256) external view returns (bool);
     function totalSupplyAt(address, uint256) external view returns (uint256);
     function balanceAt(address, address, uint256) external view returns (uint256);
     function custodianBalanceAt(address, address, address, uint256) external view returns (uint256);
@@ -28,8 +29,17 @@ contract GovernanceMinimal {
     mapping (address => mapping (bytes => bool)) approval;
     mapping (bytes32 => Proposal) proposals;
 
+    /**
+        states:
+            0 = undeclared
+            1 = pending
+            2 = active
+            3 = closed (failed to reach quorum)
+            4 = closed (did not pass)
+            5 = closed (passed)
+     */
     struct Proposal {
-        uint8 state; /** 0=undeclared, 1=pending, 2=active, 3=closed */
+        uint8 state;
         uint8 maxVoteValue;
         uint64 checkpoint;
         uint64 start;
@@ -46,7 +56,7 @@ contract GovernanceMinimal {
         uint16 requiredPct;
         uint16 quorumPct;
         uint256 totalVotes;
-        uint256[] counts;
+        uint256[2] counts;
         Token[] tokens;
     }
 
@@ -84,7 +94,6 @@ contract GovernanceMinimal {
         uint64 _start,
         uint64 _end,
         string _description,
-        uint8 _maxVoteValue,
         address _approvalAddress,
         bytes _approvalCalldata
     )
@@ -95,19 +104,15 @@ contract GovernanceMinimal {
         Proposal storage p = proposals[_id];
         require(p.state == 0);
         require(_checkpoint <= _start);
-        /* overflow protection, value cannot be 0 or 255 */
-        require(_maxVoteValue - 1 < 254);
         p.state = 1;
         p.checkpoint = _checkpoint;
         p.start = _start;
         p.end = _end;
         p.description = _description;
-        p.maxVoteValue = _maxVoteValue;
         if (_approvalAddress != 0x00) {
             p.approvalAddress = _approvalAddress;
             p.approvalCalldata = _approvalCalldata;
         }
-        /* TODO checkpoint - set it or verify it exists */
         /* TODO emit event */
         return true;
     }
@@ -125,25 +130,33 @@ contract GovernanceMinimal {
         if (!_checkPermitted()) return false;
         Proposal storage p = proposals[_id];
         require(p.state == 1);
+        require(_tokens.length > 0);
         require(_tokens.length == _multipliers.length);
         require(_requiredPct <= 10000);
+        require(_quorumPct <= 10000);
         p.votes.length += 1;
         Vote storage v = p.votes[p.votes.length-1];
         v.requiredPct = _requiredPct;
         v.quorumPct = _quorumPct;
         v.tokens.length = _tokens.length;
-        v.counts.length = p.maxVoteValue + 1;
-        /* TODO make sure no duplicate entries in tokens array */
         for (uint256 i; i < _tokens.length; i++) {
+            for (uint256 x = i + 1; x < _tokens.length; x++) {
+                require(_tokens[x] != _tokens[i]);
+            }
+            /**
+                checkpoint must exist when the vote is declared
+                possible TODO - allow this contract to create the checkpoint
+             */
+            require(checkpoint.checkpointExists(_tokens[i], p.checkpoint));
             v.tokens[i] = Token(_tokens[i], _multipliers[i]);
         }
         /* TODO emit event */
         return true;
     }
 
-    function vote(bytes32 _id, uint256 _vote) external returns (bool) {
+    function voteOnProposal(bytes32 _id, uint8 _vote) external returns (bool) {
         Proposal storage p = proposals[_id];
-        require(_vote <= p.maxVoteValue);
+        require(_vote < 2);
 
         /* query checkpoint totalSupply and set total possible votes */
         if (p.state == 1) {
@@ -171,7 +184,13 @@ contract GovernanceMinimal {
         return true;
     }
 
-    function custodialVote(bytes32 _id, address _custodian) external returns (bool) {
+    function custodialVoteForProposal(
+        bytes32 _id,
+        address _custodian
+    )
+        external
+        returns (bool)
+    {
         Proposal storage p = proposals[_id];
         require(p.state == 2);
         require(p.end >= now || p.end == 0);
@@ -180,7 +199,11 @@ contract GovernanceMinimal {
         uint256 _vote = p.hasVoted[msg.sender][0x00] - 1;
         p.hasVoted[msg.sender][_custodian] = _vote + 1;
         for (uint256 i; i < p.votes.length; i++) {
-            p.votes[i].counts[_vote] += _getCustodianVotes(p.votes[i].tokens, _custodian, p.checkpoint);
+            p.votes[i].counts[_vote] += _getCustodianVotes(
+                p.votes[i].tokens,
+                _custodian,
+                p.checkpoint
+            );
         }
         /* TODO emit event? */
         return true;
@@ -232,29 +255,51 @@ contract GovernanceMinimal {
         returns (uint256 _total)
     {
         for (uint256 i; i < t.length; i++) {
-            uint256 _bal = checkpoint.custodianBalanceAt(t[i].addr, msg.sender, _cust, _time);
+            uint256 _bal = checkpoint.custodianBalanceAt(
+                t[i].addr,
+                msg.sender,
+                _cust,
+                _time
+            );
             _total = _total.add(_bal.mul(t[i].multiplier));
         }
         return _total;
     }
 
-
-
-    function closeVote(bytes32 _id) external returns (bool) {
+    function closeProposal(bytes32 _id) external returns (bool) {
         if (!_checkPermitted()) return false;
         Proposal storage p = proposals[_id];
         require(p.state == 2);
         require(p.end < now);
-        if (p.end == 0) {
-
+        uint256 _state = 5;
+        uint256[] memory _results = new uint256[](p.votes.length);
+        for (uint256 i; i < p.votes.length; i++) {
+            _results[i] = _getVoteResult(p.votes[i]);
+            if (_results[i] < _state) {
+                _state = _results[i];
+            }
         }
-        p.state = 3;
+        /** if proposal has no end time, may only be closed once it passes */
+        if (p.end == 0) {
+            require(_state == 5);
+        }
+        p.state = uint8(_state);
         
-        /* TODO tally votes, give permission if yes */
+        if (_state == 5 && p.approvalAddress != 0x00) {
+            approval[p.approvalAddress][p.approvalCalldata] = true;
+        }
     }
 
-
-
+    function _getVoteResult(Vote storage v) internal view returns (uint8) {
+        if (v.quorumPct > 0) {
+            uint256 _total = v.counts[0].add(v.counts[1]);
+            if (_total.mul(10000).div(v.totalVotes) < v.quorumPct) return 3;
+        } else {
+            _total = v.totalVotes;
+        }
+        if (v.counts[1].mul(10000).div(_total) < v.requiredPct) return 4;
+        return 5;
+    }
 
 
     function _checkApproval() internal {
